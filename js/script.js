109a5644f075ccd2ec19db705df16107e6125817
Daniel Grigo <daniel.grigo@freenet.de>
2019-11-18 11:00:33 +0100

    All functions working, added comments

--- js/script.js
+++ js/script.js
@@ -6,29 +6,11 @@ FSJS project 2 - List Filter and Pagination
 const studentsList = document.querySelectorAll(".student-item");
 const itemsPerPage = 10;
 
-function showPage (list, page, itemsPerPage){
-  if (list.length==0){
-    noResult();
-    return
-  }
-  let startIndex = (page*itemsPerPage)-itemsPerPage;
-  let endIndex = (page*itemsPerPage)-1;
-  for (let i=0; i<list.length; i++){
-      if (i>=startIndex && i<=endIndex){
-        list[i].style.display ="";
-      } else {
-        list[i].style.display = "none";
-      }
-  }
-}
-
-function noResult (){
-  const list = document.querySelector(".student-list");
-  if (list.lastChild.className!=="no-result"){
-    const listEmpty = createElements("h2", [["className", "no-result"], ["textContent", "No results"]], list);
-  }
-}
-
+/* Creates HTML-Elements using "name" as name of the element (like "div"), "properties"
+for assigning attributes like class names or text content together with the respective
+value and "parentNode" to which the element is appended to. IMPORTANT: "properties" has to be an
+one- or two-dimensional array, each consisting of two elements in this pattern: [attribute, value].
+*/
 function createElements(name, properties, parentNode){
   const element = document.createElement(name);
   if (Array.isArray(properties[0])){
@@ -42,7 +24,23 @@ function createElements(name, properties, parentNode){
   parentNode.appendChild(element);
   return element;
 }
+// Shows only those list elements whose index is in a certain range defined by the page number & itemsPerPage
+function showPage (list, page, itemsPerPage){
+  let startIndex = (page*itemsPerPage)-itemsPerPage;
+  let endIndex = (page*itemsPerPage)-1;
+  for (let i=0; i<list.length; i++){
+      if (i>=startIndex && i<=endIndex){
+        list[i].style.display ="";
+      } else {
+        list[i].style.display = "none";
+      }
+  }
+}
 
+/* Applies the pagination links to the bottom of the page and also assigns a click event to each link.
+The number of added links is calculated by dividing the list length with the items per Page, the latter
+being a constant which was defined at the beginning of this script
+*/
 function appendPageLinks (list, itemsPerPage){
   const divPage = document.querySelector(".page");
   const divPageLinks = createElements("div", ["className", "pagination"], divPage);
@@ -54,7 +52,9 @@ function appendPageLinks (list, itemsPerPage){
       if (i===1){
         anchors.className="active";
       }
-
+/* Checks if the clicked link is showing the active page, eventually setting it's class to active
+and calling the showPage function for that page
+*/
       anchors.addEventListener("click", () => {
         if (anchors.className !== "active"){
           const allAnchors = ulPageLinks.querySelectorAll("a");
@@ -68,18 +68,34 @@ function appendPageLinks (list, itemsPerPage){
   }
 }
 
+// What the name suggests...
 function replacePageLinks(list, itemsPerPage){
   const divPage = document.querySelector(".page");
   divPage.removeChild(divPage.lastChild);
   appendPageLinks (list, itemsPerPage);
 }
 
+// Removes  "No results" from previous searches and adds it if the list of search hits is empty
+function checkResults (list){
+  const ul = document.querySelector(".student-list");
+  if (ul.lastChild.className=="no-result"){
+    ul.removeChild(ul.lastChild);
+  }
+  if (list.length==0){
+    const noResult = createElements("h2", [["className", "no-result"], ["textContent", "No results"]], ul);
+  }
+}
+
+// Adds a search bar to the document. The search is either started by clicking a button or dynamically if a key is pressed
 function appendSearchBar(){
   const listHeader = document.querySelector(".page-header");
   const divSearchBar = createElements("div", ["className", "student-search"], listHeader);
   const input = createElements("input", [["type", "text"], ["placeholder", "Search for students"]], divSearchBar);
   const button = createElements("button", ["textContent", "Search"], divSearchBar );
 
+/* Checks which student names are matching the input and adds the matches to an new list.
+Also replaces page links based on the number of hits or throw a "No results" message if nothing is found.
+*/
   button.addEventListener("click", () =>{
     if (input.value){
       const searchText = input.value.toLowerCase();
@@ -94,9 +110,28 @@ function appendSearchBar(){
               studentsList[i].style.display ="none";
             }
         }
+        checkResults(hits);
         replacePageLinks(hits, itemsPerPage);
         showPage(hits, 1, itemsPerPage);
       }
+    })
+// Same as for the buttons event listener, but on "keyup"-event so the results are shown dynamically
+  input.addEventListener("keyup", () =>{
+      const searchText = input.value.toLowerCase();
+      const studentNames = document.querySelectorAll(".student-details>h3");
+      const hits = [];
+        for (let i=0; i<studentsList.length; i++){
+          const name = studentNames[i].textContent.toLowerCase();
+            if (name.includes(searchText)){
+              studentsList[i].style.display ="";
+              hits.push(studentsList[i]);
+            } else {
+              studentsList[i].style.display ="none";
+            }
+        }
+        checkResults(hits);
+        replacePageLinks(hits, itemsPerPage);
+        showPage(hits, 1, itemsPerPage);
     })
 }
 
